c1ea15a49aeda181898efb42c9919f9b8c0b0f75
Barun Mitra <itbarunmitra1000@gmail.com>
2021-09-02 17:26:57 +0600

    add error message

--- js/book.js
+++ js/book.js
@@ -1,7 +1,7 @@
-const setNewSpinner = (displaySpinner) =>{
+const setNewSpinner = (displaySpinner) => {
     document.getElementById('loading-spinner').style.display = displaySpinner;
 };
-const setDisplayResult = (displaySpinner) =>{
+const setDisplayResult = (displaySpinner) => {
     document.getElementById('disply-result').style.display = displaySpinner;
 };
 
@@ -20,7 +20,18 @@ const getBooksData = (inputBookValue) => {
     const booksUrl = `https://openlibrary.org/search.json?q=${inputBookValue}`;
     fetch(booksUrl)
         .then(res => res.json())
-        .then(data => getBooks(data.docs));
+        .then(data => getBooks(data.docs))
+    // .catch(error => {
+    //     const errorMessage = document.getElementById('error-message');
+    //     // errorMessage.classList.remove('d-none');
+    //     alert('this')
+    //     setNewSpinner('none');
+    // // });
+    // .catch(error => {
+    //         console.log(error.message);
+    //         document.getElementById('error-message').classList.remove('d-none');
+    //         setNewSpinner('none');
+    //     })
 };
 
 
@@ -28,27 +39,28 @@ const getBooks = (books) => {
     console.log(books);
     const booksDisplay = document.getElementById('books-display');
     booksDisplay.textContent = '';
-
-    books.forEach(book => {
-        console.log(book);
-        const createDiv = document.createElement('div');
-        createDiv.classList.add('book-card');
-        createDiv.innerHTML = `
-        <img class= "book-img" src= "https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"  alt="Image Not Found">
-        <div class="card-body">
-          <h5 >${book.title}</h5>
-          <h6 >By ${book.author_name}</h6>
-          <p >Publish: ${book.first_publish_year}</p>         
-        `;
-        booksDisplay.appendChild(createDiv);
-       
-
-    });
-    
-    const totalFound = document.getElementById('total-found');
-    totalFound.innerHTML = `Total Found Result: ${books.length}`;
-    setNewSpinner('none');
-    setDisplayResult('block');
-
-
+    if (books.length === 0) {
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.classList.remove('d-none');
+        setNewSpinner('none');
+    } else {
+        books.forEach(book => {
+            console.log(book);
+            const createDiv = document.createElement('div');
+            createDiv.classList.add('book-card');
+            createDiv.innerHTML = `
+            <img class= "book-img" src= "https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"  alt="Image Not Found">
+            <div class="card-body">
+              <h5 >${book.title}</h5>
+              <h6 >By ${book.author_name}</h6>
+              <p >Publish: ${book.first_publish_year}</p>         
+            `;
+            booksDisplay.appendChild(createDiv);
+            const totalFound = document.getElementById('total-found');
+            totalFound.innerHTML = `Total Found Result: ${books.length}`;
+        });
+        setNewSpinner('none');
+        setDisplayResult('block');
+    }
+ 
 };
